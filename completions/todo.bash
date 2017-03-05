cc11a750466756ad5319693506090ba497a13b45
Marc Neudert <marc.neudert@gmail.com>
2017-03-05 15:49:28 +0100

    ws / cs

--- completions/todo.bash
+++ completions/todo.bash
@@ -4,17 +4,16 @@ _todo()
 {
   COMPREPLY=()
 
-  cur=${COMP_WORDS[COMP_CWORD]}
-  prev=${COMP_WORDS[COMP_CWORD-1]}
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  local prev=${COMP_WORDS[COMP_CWORD-1]}
 
-  # Complete all commands
-  opts='backup clear count list modify rm swap'
+  local commands='backup clear count list modify rm swap'
 
-  # Only complete on the first term
+  # Complete on the first term (command)
   if [ $COMP_CWORD -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
     return 0
   fi
-
 }
+
 complete -F _todo todo
